f02f6d994e165bf55ea6a05a6461f1880b461a71
thassiov <tvmcarvalho@gmail.com>
2024-09-25 22:41:01 -0300

    big bang

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+// @ts-expect-error i dont know how to fix it yet
+import eslintJest from 'eslint-plugin-jest';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    files: ['**/*/*.spec.ts', '**/*/*.spec.ts'],
+    plugins: { eslintJest },
+  }
+);
