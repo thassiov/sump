b3d7001064a470d71716bac2c964d9b2f8e2cf3a
thassiov <tvmcarvalho@gmail.com>
2025-01-23 15:42:56 -0300

    Removes old files

--- src/repositories/profile-retrieve/types/index.ts
+++
@@ -1,7 +0,0 @@
-import { IProfile } from '../../../types/account.type';
-
-type IProfileRetrieveRepository = {
-  retrieveByAccountId: (accountId: string) => Promise<IProfile | null>;
-};
-
-export type { IProfileRetrieveRepository };
--- src/services/profile-retrieve/types/index.ts
+++
@@ -1,7 +0,0 @@
-import { IProfile } from '../../../types/account.type';
-
-type IProfileRetrieveService = {
-  retrieveByAccountId: (accountId: string) => Promise<IProfile | null>;
-};
-
-export type { IProfileRetrieveService };
