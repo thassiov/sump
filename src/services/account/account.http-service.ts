a914f1a3e6d26ed2b7e70c9c84a8935c45abad10
thassiov <tvmcarvalho@gmail.com>
2025-09-04 13:55:34 -0300

    Adjust account service's endpoint and http service

--- src/services/account/account.http-service.ts
+++ src/services/account/account.http-service.ts
@@ -1,6 +1,14 @@
 import { BaseHttpService } from '../../base-classes';
+import { IUpdateTenantNonSensitivePropertiesDto } from '../tenant/types/dto.type';
+import { ITenant } from '../tenant/types/tenant.type';
 import { IAccount } from './types/account.type';
-import { ICreateAccountDto, IUpdateAccountDto } from './types/dto.type';
+import {
+  ICreateAccountDto,
+  IGetAccountDto,
+  IUpdateAccountEmailDto,
+  IUpdateAccountPhoneDto,
+  IUpdateAccountUsernameDto,
+} from './types/dto.type';
 import { IAccountService } from './types/service.type';
 
 class AccountHttpService extends BaseHttpService implements IAccountService {
@@ -8,24 +16,61 @@ class AccountHttpService extends BaseHttpService implements IAccountService {
     super('account-http-service', url);
   }
 
-  async create(dto: ICreateAccountDto): Promise<string> {
-    return await this.httpClient.post<string>(this.serviceUrl, {
+  async create(
+    tenantId: ITenant['id'],
+    dto: ICreateAccountDto
+  ): Promise<string> {
+    const url = `${this.serviceUrl}?tenantId=${tenantId}`;
+    return await this.httpClient.post<string>(url, {
       body: dto,
     });
   }
 
-  async getById(id: string): Promise<IAccount | undefined> {
+  async getById(id: IAccount['id']): Promise<IGetAccountDto | undefined> {
     const url = `${this.serviceUrl}/${id}`;
-    return await this.httpClient.get<IAccount | undefined>(url);
+    return await this.httpClient.get<IGetAccountDto | undefined>(url);
   }
 
-  async deleteById(id: string): Promise<boolean> {
+  async deleteById(id: IAccount['id']): Promise<boolean> {
     const url = `${this.serviceUrl}/${id}`;
     return await this.httpClient.delete<boolean>(url);
   }
 
-  async updateById(id: string, dto: IUpdateAccountDto): Promise<boolean> {
+  async updateNonSensitivePropertiesById(
+    id: string,
+    dto: IUpdateTenantNonSensitivePropertiesDto
+  ): Promise<boolean> {
     const url = `${this.serviceUrl}/${id}`;
+    return await this.httpClient.patch<boolean>(url, {
+      body: dto,
+    });
+  }
+
+  async updateEmailById(
+    id: IAccount['id'],
+    dto: IUpdateAccountEmailDto
+  ): Promise<boolean> {
+    const url = `${this.serviceUrl}/${id}/email`;
+    return await this.httpClient.patch<boolean>(url, {
+      body: dto,
+    });
+  }
+
+  async updatePhoneById(
+    id: IAccount['id'],
+    dto: IUpdateAccountPhoneDto
+  ): Promise<boolean> {
+    const url = `${this.serviceUrl}/${id}/phone`;
+    return await this.httpClient.patch<boolean>(url, {
+      body: dto,
+    });
+  }
+
+  async updateUsernameById(
+    id: IAccount['id'],
+    dto: IUpdateAccountUsernameDto
+  ): Promise<boolean> {
+    const url = `${this.serviceUrl}/${id}/username`;
     return await this.httpClient.patch<boolean>(url, {
       body: dto,
     });
--- src/services/account/account.routes.ts
+++ src/services/account/account.routes.ts
@@ -2,8 +2,14 @@ import express, { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 // import { setupLogger } from '../../lib/logger/logger';
 import { EndpointHandler } from '../../lib/types';
+import {
+  IUpdateTenantEnvironmentAccountEmailDto,
+  IUpdateTenantEnvironmentAccountNonSensitivePropertiesDto,
+  IUpdateTenantEnvironmentAccountPhoneDto,
+  IUpdateTenantEnvironmentAccountUsernameDto,
+} from '../tenant-environment-account/types/dto.type';
 import { AccountService } from './account.service';
-import { ICreateAccountDto, IUpdateAccountDto } from './types/dto.type';
+import { ICreateAccountDto } from './types/dto.type';
 
 const router = express.Router();
 // const logger = setupLogger('enpoint-v1-accounts');
@@ -13,7 +19,19 @@ function makeServiceEndpoints(accountService: AccountService): express.Router {
   router.get('/v1/accounts/:id', makeGetByIdEndpointFactory(accountService));
   router.patch(
     '/v1/accounts/:id',
-    makeUpdateByIdEndpointFactory(accountService)
+    makeUpdateNonSensitivePropertiesByIdEndpointFactory(accountService)
+  );
+  router.patch(
+    '/v1/accounts/:id/email',
+    makeUpdateEmailByIdEndpointFactory(accountService)
+  );
+  router.patch(
+    '/v1/accounts/:id/phone',
+    makeUpdatePhoneByIdEndpointFactory(accountService)
+  );
+  router.patch(
+    '/v1/accounts/:id/username',
+    makeUpdateUsernameByIdEndpointFactory(accountService)
   );
   router.delete(
     '/v1/accounts/:id',
@@ -30,7 +48,11 @@ function makeCreateEndpointFactory(
     req: Request,
     res: Response
   ): Promise<void> {
-    const id = await accountService.create(req.body as ICreateAccountDto);
+    const tenantId = req.query['tenantId'] as string;
+    const id = await accountService.create(
+      tenantId,
+      req.body as ICreateAccountDto
+    );
 
     res.status(StatusCodes.CREATED).json({ id });
     return;
@@ -58,17 +80,87 @@ function makeGetByIdEndpointFactory(
   };
 }
 
-function makeUpdateByIdEndpointFactory(
+function makeUpdateNonSensitivePropertiesByIdEndpointFactory(
   accountService: AccountService
 ): EndpointHandler {
-  return async function makeUpdateByIdEndpoint(
+  return async function makeUpdateNonSensitivePropertiesByIdEndpoint(
     req: Request,
     res: Response
   ): Promise<void> {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const id = req.params['id']!;
-    const dto = req.body as IUpdateAccountDto;
-    const account = await accountService.updateById(id, dto);
+    const dto =
+      req.body as IUpdateTenantEnvironmentAccountNonSensitivePropertiesDto;
+    const account = await accountService.updateNonSensitivePropertiesById(
+      id,
+      dto
+    );
+
+    if (!account) {
+      res.status(StatusCodes.NOT_FOUND).send();
+      return;
+    }
+
+    res.status(StatusCodes.OK).send();
+    return;
+  };
+}
+
+function makeUpdateEmailByIdEndpointFactory(
+  accountService: AccountService
+): EndpointHandler {
+  return async function makeUpdateEmailByIdEndpoint(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const id = req.params['id']!;
+    const dto = req.body as IUpdateTenantEnvironmentAccountEmailDto;
+    const account = await accountService.updateEmailById(id, dto);
+
+    if (!account) {
+      res.status(StatusCodes.NOT_FOUND).send();
+      return;
+    }
+
+    res.status(StatusCodes.OK).send();
+    return;
+  };
+}
+
+function makeUpdatePhoneByIdEndpointFactory(
+  accountService: AccountService
+): EndpointHandler {
+  return async function makeUpdatePhoneByIdEndpoint(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const id = req.params['id']!;
+    const dto = req.body as IUpdateTenantEnvironmentAccountPhoneDto;
+    const account = await accountService.updatePhoneById(id, dto);
+
+    if (!account) {
+      res.status(StatusCodes.NOT_FOUND).send();
+      return;
+    }
+
+    res.status(StatusCodes.OK).send();
+    return;
+  };
+}
+
+function makeUpdateUsernameByIdEndpointFactory(
+  accountService: AccountService
+): EndpointHandler {
+  return async function makeUpdateUsernameByIdEndpoint(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const id = req.params['id']!;
+    const dto = req.body as IUpdateTenantEnvironmentAccountUsernameDto;
+    const account = await accountService.updateUsernameById(id, dto);
 
     if (!account) {
       res.status(StatusCodes.NOT_FOUND).send();
