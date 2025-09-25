2cf9b83389741ce44b800c0783193e4fe0cc22a4
thassiov <tvmcarvalho@gmail.com>
2025-09-25 16:18:02 -0300

    Removes old modules

--- src/services/index.ts
+++
@@ -1,6 +0,0 @@
-import { account } from './account';
-import { tenant } from './tenant';
-import { tenantEnvironment } from './tenant-environment';
-import { tenantEnvironmentAccount } from './tenant-environment-account';
-
-export { account, tenant, tenantEnvironment, tenantEnvironmentAccount };
