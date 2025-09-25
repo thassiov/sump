b3ae969b90e5ec9eeceed4dc4a4f31b1b52aca70
thassiov <tvmcarvalho@gmail.com>
2025-09-25 00:52:18 -0300

    Adds user case endpoints for account

--- src/infra/rest-api/endpoints/account.endpoint.ts
+++ src/infra/rest-api/endpoints/account.endpoint.ts
@@ -1,7 +1,13 @@
 /* eslint-disable @typescript-eslint/non-nullable-type-assertion-style */
 import express, { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { ICreateAccountDto } from '../../../core/types/account/dto.type';
+import {
+  IAccountUserDefinedIdentification,
+  ICreateAccountDto,
+  IUpdateAccountEmailDto,
+  IUpdateAccountPhoneDto,
+  IUpdateAccountUsernameDto,
+} from '../../../core/types/account/dto.type';
 import { IUpdateTenantNonSensitivePropertiesDto } from '../../../core/types/tenant/dto.type';
 import { AccountUseCase } from '../../../core/use-cases';
 import { EndpointHandler } from '../../../lib/types';
@@ -25,6 +31,40 @@ function makeAccountUseCaseEndpoints(useCase: AccountUseCase): express.Router {
     '/accounts/:accountId',
     updateNonSensitivePropertiesByIdAndTenantIdUseCaseEndpointFactory(
       useCase.updateNonSensitivePropertiesByIdAndTenantId
+    )
+  );
+
+  router.patch(
+    '/accounts/:accountId/email',
+    updateAccountEmailByIdAndTenantIdUseCaseEndpointFactory(
+      useCase.updateAccountEmailByIdAndTenantId
+    )
+  );
+
+  router.patch(
+    '/accounts/:accountId/phone',
+    updateAccountPhoneByIdAndTenantIdUseCaseEndpointFactory(
+      useCase.updateAccountPhoneByIdAndTenantId
+    )
+  );
+  router.patch(
+    '/accounts/:accountId/username',
+    updateAccountUsernameByIdAndTenantIdUseCaseEndpointFactory(
+      useCase.updateAccountUsernameByIdAndTenantId
+    )
+  );
+
+  router.get(
+    '/accounts/user-defined-identification',
+    getAccountByUserDefinedIdentificationAndTenantIdUseCaseEndpointFactory(
+      useCase.getAccountByUserDefinedIdentificationAndTenantId
+    )
+  );
+
+  router.get(
+    '/accounts/:accountId',
+    getAccountByIdAndTenantIdUseCaseEndpointFactory(
+      useCase.getAccountByIdAndTenantId
     )
   );
 
@@ -76,6 +116,100 @@ function updateNonSensitivePropertiesByIdAndTenantIdUseCaseEndpointFactory(
     await useCase(accountId, tenantId, dto);
 
     res.status(StatusCodes.OK).send();
+    return;
+  };
+}
+
+function updateAccountEmailByIdAndTenantIdUseCaseEndpointFactory(
+  useCase: AccountUseCase['updateAccountEmailByIdAndTenantId']
+): EndpointHandler {
+  return async function updateAccountEmailByIdAndTenantIdUseCaseEndpoint(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    const tenantId = req.params['tenantId'] as string;
+    const accountId = req.params['accountId'] as string;
+    const dto = req.body as IUpdateAccountEmailDto;
+    await useCase(accountId, tenantId, dto);
+
+    res.status(StatusCodes.OK).send();
+    return;
+  };
+}
+
+function updateAccountPhoneByIdAndTenantIdUseCaseEndpointFactory(
+  useCase: AccountUseCase['updateAccountPhoneByIdAndTenantId']
+): EndpointHandler {
+  return async function updateAccountPhoneByIdAndTenantIdUseCaseEndpoint(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    const tenantId = req.params['tenantId'] as string;
+    const accountId = req.params['accountId'] as string;
+    const dto = req.body as IUpdateAccountPhoneDto;
+    await useCase(accountId, tenantId, dto);
+
+    res.status(StatusCodes.OK).send();
+    return;
+  };
+}
+
+function updateAccountUsernameByIdAndTenantIdUseCaseEndpointFactory(
+  useCase: AccountUseCase['updateAccountUsernameByIdAndTenantId']
+): EndpointHandler {
+  return async function updateAccountUsernameByIdAndTenantIdUseCaseEndpoint(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    const tenantId = req.params['tenantId'] as string;
+    const accountId = req.params['accountId'] as string;
+    const dto = req.body as IUpdateAccountUsernameDto;
+    await useCase(accountId, tenantId, dto);
+
+    res.status(StatusCodes.OK).send();
+    return;
+  };
+}
+
+function getAccountByIdAndTenantIdUseCaseEndpointFactory(
+  useCase: AccountUseCase['getAccountByIdAndTenantId']
+): EndpointHandler {
+  return async function getAccountByIdAndTenantIdUseCaseEndpoint(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    const tenantId = req.params['tenantId'] as string;
+    const accountId = req.params['accountId'] as string;
+    const account = await useCase(accountId, tenantId);
+
+    if (!account) {
+      res.status(StatusCodes.NOT_FOUND).send();
+      return;
+    }
+
+    res.status(StatusCodes.OK).json(account);
+    return;
+  };
+}
+
+function getAccountByUserDefinedIdentificationAndTenantIdUseCaseEndpointFactory(
+  useCase: AccountUseCase['getAccountByUserDefinedIdentificationAndTenantId']
+): EndpointHandler {
+  return async function getAccountByUserDefinedIdentificationAndTenantIdUseCaseEndpoint(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    const tenantId = req.params['tenantId'] as string;
+    const dto = req.body as IAccountUserDefinedIdentification;
+
+    const account = await useCase(dto, tenantId);
+
+    if (!account) {
+      res.status(StatusCodes.NOT_FOUND).send();
+      return;
+    }
+
+    res.status(StatusCodes.OK).json(account);
     return;
   };
 }
